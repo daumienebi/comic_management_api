4d88b1bb0f0eb3bbb651e22c7c4b7deb6d486333
daumienebi <ddsakpa@gmail.com>
2022-06-05 17:40:27 +0200

    Created the Springboot project with Springboot inicializer

---
+++ src/main/java/es/daumienebi/comic_management_server/ComicManagementServerApplication.java
@@ -0,0 +1,13 @@
+package es.daumienebi.comic_management_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicManagementServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicManagementServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/daumienebi/comic_management_server/ComicManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.daumienebi.comic_management_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicManagementServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
