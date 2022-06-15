50907d7af84c39a16d47a28c7acf8044f44589a0
daumienebi <ddsakpa@gmail.com>
2022-06-15 05:55:42 +0200

    feat: comics can now be updated

--- src/main/java/es/daumienebi/comic_management_server/comic/ComicController.java
+++ src/main/java/es/daumienebi/comic_management_server/comic/ComicController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -29,13 +31,19 @@ public class ComicController {
 		return comicService.findComicById(id);
 	}
 	
-	@DeleteMapping(path="/{id}")
+	@RequestMapping(path="/{id}",method = RequestMethod.DELETE)
 	public boolean deleteComicById(@PathVariable("id") Long id) {
 		return comicService.deleteComicById(id);
 	}
 	
-	@PostMapping("/comics")
+	@PostMapping(name = "/comics")
 	public Comic saveComic(@RequestBody Comic comic) {
 		return comicService.saveComic(comic);
+	}
+	
+	@RequestMapping(path = "/{id}",method = RequestMethod.PUT)
+	public Comic updateComic(@RequestBody Comic newComic, @PathVariable Long id) {
+		
+		return comicService.updateComic(newComic);
 	}
 }
